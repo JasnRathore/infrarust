b9b37919a64731255c9a7d2f8292e5b153710562
Jasn Rathore <139693551+JasnRathore@users.noreply.github.com>
2025-07-24 22:11:05 +0530

    added shell execution

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use infra::shell::ShellCommandDetector;
 
+use std::process::Command;
+use std::env;
+use std::path::Path;
+use std::cell::RefCell;
+use std::rc::Rc;
+
 use rustyline::Context;
 use rustyline::completion::{Completer, Pair};
 use rustyline::highlight::Highlighter;
 use rustyline::hint::Hinter;
 use rustyline::validate::{ValidationContext, ValidationResult, Validator};
 use rustyline::{Editor, Helper};
-
-use std::cell::RefCell;
-use std::rc::Rc;
+use hostname::get;
 
 struct ShellCommandHinter {
     detector: Rc<RefCell<ShellCommandDetector>>,
@@ -87,8 +91,20 @@ fn main() -> rustyline::Result<()> {
 
     println!("Enter commands to check (type 'exit' to quit):");
 
+    let username = env::var("USER").unwrap_or_else(|_| "user".to_string());
+    let hostname = get().unwrap_or_default().into_string().unwrap_or_default();
+
     loop {
-        let readline = rl.readline(">> ");
+        let current_dir = env::current_dir().unwrap_or_else(|_| ".".into());
+        // ANSI color codes:
+        // Red: \x1b[31m
+        // Green: \x1b[32m
+        // Yellow: \x1b[33m
+        // Blue: \x1b[34m
+        // Reset: \x1b[0m
+        let prompt = format!("\x1b[32m{}@{}\x1b[0m:\x1b[34m{}\x1b[0m\n\x1b[31minfraware\x1b[0m$ ", username, hostname, current_dir.display());
+        let readline = rl.readline(&prompt);
+
         match readline {
             Ok(line) => {
                 let input = line.trim();
@@ -96,7 +112,38 @@ fn main() -> rustyline::Result<()> {
                     break;
                 }
                 let is_shell_command = detector.borrow_mut().is_shell_command(input);
+
                 println!("Is shell command: {}", is_shell_command);
+                if !is_shell_command {
+                    continue;
+                }
+
+                let mut parts = input.trim().split_whitespace();
+
+                let command = match parts.next() {
+                    Some(command) => command,
+                    None => continue, // skip to next iteration if None
+                };
+                let args = parts;
+                match command {
+                    "cd" => {
+                        let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                        let root = Path::new(new_dir);
+                        if let Err(e) = env::set_current_dir(&root) {
+                            eprintln!("CD error: {e}");
+                        };
+                        continue;
+                    },
+                    _ => {},
+                }
+                let mut child = match Command::new(command).args(args).spawn() {
+                    Ok(child) => child,
+                    Err(e) => {
+                        eprintln!("Application error: {e}");
+                        continue;
+                    },
+                };
+                let _ = child.wait();
             }
             Err(_) => println!("No Input"),
         }
